decdf5198b21377281e4153ef31f2aaf82f37ba9
TheProgrammer33 <dylan.m.bryant@gmail.com>
2020-02-13 15:38:58 -0500

    stuff

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-angular.module('', ['ui.bootstrap']);
+function myFunction(){
+	var email = 'dylan.m.bryant@gmail.com';
+	var password = 'Password';
+	this.getElementById("email").innerHTML = email;
+}
+
+function adminView(){
+	document.getElementById("demo").innerHTML = "This is still the admin page";
+}
