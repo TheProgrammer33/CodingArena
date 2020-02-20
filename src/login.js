b798de809223c05a7b73ace3e589249409e8a0e9
TheProgrammer33 <dylan.m.bryant@gmail.com>
2020-02-20 14:07:04 -0500

    Login

--- src/login.js
+++ src/login.js
@@ -1,22 +1,31 @@
 function start(){
 	var app = angular.module('arenaPage', []);
 	app.controller('selectCtrl', function($scope) {
-    	//$scope.signindiv = "<form name='roleForm'><select name='roleSelector' ng-model='selectedRole' ng-options='x for x in compRole'></select> <br></form><button ng-disabled='roleForm.roleSelector.$pristine' ng-click='changePage()''>Continue</button>";
+    	$scope.changePage =  function() {
+	    	app.directive("signin", function() {
+	    		return {
+					restrict : "A",
+					template : '<p>Test</p>'
+				};
+	    	});
+    	}
     	$scope.compRole = ["Admin", "Competitor"];
     });
-}
-/*
-function changePage(){
-	adminLogin();
-}
-
-function adminLogin(){
-	var app = angular.module('arenaPage', ['ngSanitize']);
-	app.controller('selectCtrl', function($scope) {
-      $scope.signindiv = "<p>AI will destroy everyone</p>";
+    app.directive("signin", function() {
+    	return {
+			restrict : "A",
+			template : '<form name="roleForm"><select name="roleSelector" ng-model="selectedRole" ng-options="x for x in compRole"></select> <br></form><button ng-disabled="roleForm.roleSelector.$pristine" ng-click="changePage()">Continue</button>'
+		};
     });
-
 }
-*/
+
+function adminLogin() {
+	var app1 = angular.module('arenaPage', []);
+	app1.directive("signin", function() {
+		
+	});
+}
 
 start();
+
+
