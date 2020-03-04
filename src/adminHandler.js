05392a0fa7bf7b285b089f51d493bcbf8a038a19
TheProgrammer33 <dylan.m.bryant@gmail.com>
2020-03-04 18:19:19 -0500

    Added Pages

---
+++ src/adminHandler.js
@@ -0,0 +1,11 @@
+function addQuestion() {
+	console.log("Top");
+	var questionTemplate = document.getElementById("questionTemplate");
+	var container = document.getElementById("questions");
+	container.appendChild(questionTemplate);
+	console.log("bottom");
+}
+
+function removeQuestion() {
+
+}
