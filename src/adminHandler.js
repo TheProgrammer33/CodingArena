4317482b1f0511c2544d8c4d1daa5a595dc8b6d0
TheProgrammer33 <dylan.m.bryant@gmail.com>
2020-03-05 15:41:59 -0500

    AdminCompPage
    Admins are now able to add questions and answers

--- src/adminHandler.js
+++ src/adminHandler.js
@@ -1,11 +1,29 @@
-function addQuestion() {
+function addQuestions() {
 	console.log("Top");
-	var questionTemplate = document.getElementById("questionTemplate");
+	/*var questionTemplate = document.getElementById("questionTemplate");
 	var container = document.getElementById("questions");
-	container.appendChild(questionTemplate);
-	console.log("bottom");
+	container.appendChild(questionTemplate);*/
+	angular.module("arenaPage", [])
+  	.controller("myCtrl", function($scope, $compile) {
+	    $scope.test = function() {
+	      var element = angular.element($('questions'));
+	      element.html('<p>Replaced Text</p>');
+	      $compile(element)($scope);
+	    };
+  });
+	console.log("Bottom");
 }
 
 function removeQuestion() {
 
 }
+
+
+angular.module("arenaPage", [])
+  	.controller("selectCtrl", function($scope) {
+  		$scope.questionNum = 2;
+	    $scope.addQuestion = function() {
+			$('#questions').append('<div><h2>Question</h2><textarea placeholder="Enter question here..."></textarea><br><textarea placeholder="Enter expected output here..."></textarea><br></div>');
+			$scope.questionNum = $scope.questionNum + 1;
+	    };
+  });
