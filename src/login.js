158fb884d61f0f4a7aaad237bdf39f89621d8120
TheProgrammer33 <dylan.bryant33@yahoo.com>
2020-03-11 12:16:42 -0400

    LeftOver Changes
    from my desktop, minor changes, should be overwritten by last push

--- src/login.js
+++ src/login.js
@@ -1,80 +1,44 @@
-function start(){
-	var app = angular.module("arenaPage", [])
-    app.controller('selectCtrl', function($scope) {
-    	$scope.compRole = ["Admin", "Competitor"];
-    });
+function start() {
+angular.module('loginPage', [])
+  .directive('myTabs', function() {
+    return {
+      restrict: 'E',
+      transclude: true,
+      scope: {},
+      controller: ['$scope', function MyTabsController($scope) {
+        var panes = $scope.panes = [];
+
+        $scope.select = function(pane) {
+          angular.forEach(panes, function(pane) {
+            pane.selected = false;
+          });
+          pane.selected = true;
+        };
+
+        this.addPane = function(pane) {
+          if (panes.length === 0) {
+            $scope.select(pane);
+          }
+          panes.push(pane);
+        };
+      }],
+      templateUrl: 'my-tabs.html'
+    };
+  })
+  .directive('myPane', function() {
+    return {
+      require: '^^myTabs',
+      restrict: 'E',
+      transclude: true,
+      scope: {
+        title: '@'
+      },
+      link: function(scope, element, attrs, tabsCtrl) {
+        tabsCtrl.addPane(scope);
+      },
+      templateUrl: 'my-pane.html'
+    };
+  });
 }
-/*
-function adminLogin() {
-	var app1 = angular.module('arenaPage', []);
-	app1.directive("signin", function() {
-		
-	});
-}*/
 
 start();
-
-
-/*
-
-function start(){
-    console.log("Start");
-	var app = angular.module("arenaPage", [])
-	app.directive("signin", function() {
-		return {
-			restrict : "A",
-            template : '<form name="roleForm"><select name="roleSelector" ng-model="selectedRole" ng-options="x for x in compRole"></select> <br></form><button ng-disabled="roleForm.roleSelector.$pristine" ng-click="changePage()">Continue</button>'	
-		};
-	});
-    app.controller('selectCtrl', function($scope) {
-    	$scope.compRole = ["Admin", "Competitor"];
-    });
-    console.log("Start over");
-}
-function changePage(){
-	console.log("Here");
-}
-
-
-
-
-angular.module('docsIsoFnBindExample', [])
-.controller('Controller', ['$scope', '$timeout', function($scope, $timeout) {
-  $scope.name = 'Tobias';
-  $scope.message = '';
-  $scope.hideDialog = function(message) {
-    $scope.message = message;
-    $scope.dialogIsHidden = true;
-    $timeout(function() {
-      $scope.message = '';
-      $scope.dialogIsHidden = false;
-    }, 2000);
-  };
-}])
-.directive('myDialog', function() {
-  return {
-    restrict: 'E',
-    transclude: true,
-    scope: {
-      'close': '&onClose'
-    },
-    templateUrl: 'my-dialog-close.html'
-  };
-});
-
-
-<div class="alert">
-  <a href class="close" ng-click="close({message: 'closing for now'})">&times;</a>
-  <div ng-transclude></div>
-</div>
-
-
-<div ng-controller="Controller">
-  {{message}}
-  <my-dialog ng-hide="dialogIsHidden" on-close="hideDialog(message)">
-    Check out the contents, {{name}}!
-  </my-dialog>
-</div>
-
-*/
-
