6a2d03184dd9ca04bb510cab8d16e309384e4519
TheProgrammer33 <dylan.m.bryant@gmail.com>
2020-03-11 12:12:16 -0400

    Major Update
    Properly used angular, restructured html files, removed unnecessary css

--- src/adminHandler.js
+++ src/adminHandler.js
@@ -1,29 +1,38 @@
-function addQuestions() {
-	console.log("Top");
-	/*var questionTemplate = document.getElementById("questionTemplate");
-	var container = document.getElementById("questions");
-	container.appendChild(questionTemplate);*/
-	angular.module("arenaPage", [])
-  	.controller("myCtrl", function($scope, $compile) {
-	    $scope.test = function() {
-	      var element = angular.element($('questions'));
-	      element.html('<p>Replaced Text</p>');
-	      $compile(element)($scope);
-	    };
-  });
-	console.log("Bottom");
-}
-
-function removeQuestion() {
-
-}
-
-
 angular.module("arenaPage", [])
-  	.controller("selectCtrl", function($scope) {
-  		$scope.questionNum = 2;
-	    $scope.addQuestion = function() {
-			$('#questions').append('<div><h2>Question</h2><textarea placeholder="Enter question here..."></textarea><br><textarea placeholder="Enter expected output here..."></textarea><br></div>');
-			$scope.questionNum = $scope.questionNum + 1;
-	    };
-  });
+  	.controller("questionCtrl", function($scope, $compile) {
+  		var _this = this;
+
+  		_this.questions = [];
+
+  		_this.addQuestion = function() {
+  			_this.questions.push({});
+  		};
+
+  		// Add initial question
+  		_this.addQuestion();
+
+  		_this.saveEverything = function() {
+  			console.info(_this.questions);
+  		}
+  		_this.removeQuestion = function(index) {
+  			_this.questions.splice(index, 1)
+  		}
+  })
+	.directive("fileread", function () {
+	    return {
+	        scope: {
+	            fileread: "="
+	        },
+	        link: function (scope, element, attributes) {
+	            element.bind("change", function (changeEvent) {
+	                var reader = new FileReader();
+	                reader.onload = function (loadEvent) {
+	                    scope.$apply(function () {
+	                        scope.fileread = loadEvent.target.result;
+	                    });
+	                }
+	                reader.readAsDataURL(changeEvent.target.files[0]);
+	            });
+	        }
+	    }
+	});
--- src/login.js
+++ src/login.js
@@ -1,53 +1,31 @@
 function start(){
-	var app = angular.module("arenaPage", [])
-    app.controller('selectCtrl', function($scope) {
-    	$scope.compRole = ["Admin", "Competitor"];
+	var app = angular.module("loginPage", [])
+    app.controller('loginCtrl', function($scope) {
+      var _this = this;
+
+    	_this.compRole = [{label:"Admin", isAdmin: true}, {label:"Competitor"}];
+      //TODO - Delete test data from teams list
+      _this.teams = [{teamName: 'PB and J'}, {teamName: 'NerdAlert'}, {teamName: 'US Navy'}];
+
+      _this.selectRole = function(role) {
+        if (role && role.isAdmin) {
+          _this.state = 'ADMIN_LOGIN';
+        } else if (role) {
+          _this.state = 'ENTER_CODE';
+        }
+      };
+
+      _this.competitorSteps = function(nextStep) {
+        if (nextStep === 'code_entered'){
+          //TODO - check api for valid code, if valid, do following
+          _this.state = 'PICK_TEAM';
+          //TODO - get the teams for this competition from database
+          
+        }
+      }
+
     });
   
 }
 
 start();
-
-/*
-
-app.directive('switchPage', function() {
-      return {
-      restrict: 'E',
-      transclude: true,
-      scope: {},
-      controller: ['$scope', function MyTabsController($scope) {
-        var panes = $scope.panes = [];
-
-        $scope.select = function(pane) {
-          angular.forEach(panes, function(pane) {
-            pane.selected = false;
-          });
-          pane.selected = true;
-        };
-
-        this.addPane = function(pane) {
-          if (panes.length === 0) {
-            $scope.select(pane);
-          }
-          panes.push(pane);
-        };
-      }],
-      templateUrl: 'my-tabs.html'
-    };
-  });
-  app.directive('signinPage', function() {
-      return {
-      require: '^^switchPage',
-      restrict: 'E',
-      transclude: true,
-      scope: {
-        title: '@'
-      },
-      link: function(scope, element, attrs, tabsCtrl) {
-        tabsCtrl.addPane(scope);
-      },
-      templateUrl: 'my-pane.html'
-    };
-  });
-
-*/
